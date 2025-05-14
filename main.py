4513bb1f0445361a1411cd04aa296800caa284be
Armaan Irani <armaanirani@gmail.com>
2025-05-14 10:46:07 +0530

    defined the main function

--- main.py
+++ main.py
@@ -21,3 +21,15 @@ agent = Agent(
     api_key=GROQ_API_KEY,
     mcp_server=[mcp_fetch_server],
 )
+
+# Defining the main function
+async def main():
+    async with agent.run_mcp_servers():
+        result = await agent.run("Extract the content and summarize it: https://www.turing.com/resources/finetuning-large-language-models")
+        output = result.output
+        return output
+
+# Running the main function
+if __name__ == "__main__":
+    output = asyncio.run(main())
+    print(output)
